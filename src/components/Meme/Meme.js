eafdda122e357fca78876c4033cf5e981628e8cc
Olesia Polevoi <olesia.polevoi@gmail.com>
2022-09-12 22:31:22 -0400

    added random image from 100 imgs in array

--- src/components/Meme/Meme.js
+++ src/components/Meme/Meme.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Meme.css";
+import memesData from "../memesData";
 
 export default function Meme() {
+  const [memeImage, setMemeImage] = useState("");
+
+  const getMemeImage = () => {
+    const randomIndx = Math.floor(Math.random() * 100) + 1;
+    // console.log(memesData.data.memes[randomIndx].url);
+    setMemeImage(memesData.data.memes[randomIndx].url);
+    console.log(memeImage);
+  };
+
   return (
     <main>
       <div className="form">
         <input type="text" placeholder="shut up" />
         <input type="text" placeholder="and take my money" />
-        <button type="submit">Get a new meme image 🖼</button>
+        <button onClick={getMemeImage}>Get a new meme image 🖼</button>
       </div>
+      <img src={memeImage} alt="meme" />
     </main>
   );
 }
